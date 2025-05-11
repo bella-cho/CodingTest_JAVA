1356d4acd2712dd37dce356591bad6e05711057e
Hyunjoung Cho <joungjoung614@gmail.com>
2025-05-11 21:53:30 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(int n1, int n2) {
         int answer = n1*1000 / n2;
+        return answer;
+    }
+}
+
+class Solution {
+    public int solution(int n1, int n2) {
+        double answer = (double)n1/n2 * 1000;
         return (int)answer;
     }
 }
