538b829627d74f070acbded3160375e713574865
Hyunjoung Cho <joungjoung614@gmail.com>
2025-05-13 02:07:45 +0900

    Update 짝수는 싫어요.java

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -15,3 +15,18 @@ class Solution {
         return answer;
     }
 }
+
+
+class Solution2 {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+
+        for(int i=1; i<=n; i++){
+          if(i%2 != 0) {
+              answer.add(i);
+          } 
+        }
+
+        return answer;
+    }
+}
