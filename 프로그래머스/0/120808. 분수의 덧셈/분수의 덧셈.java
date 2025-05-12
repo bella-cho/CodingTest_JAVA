89b0a02077e428a7e5b393053ec12140ae672678
Hyunjoung Cho <joungjoung614@gmail.com>
2025-05-12 15:01:25 +0900

    [level 0] Title: 분수의 덧셈, Time: 10.32 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -2,10 +2,11 @@ class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
         
+        //분수 나오면 통분하기 !
         int d = denom1*denom2;
         int n = numer1*denom2 + numer2*denom1;
         int max = 1;
-        
+        //최대공약수 구해 최대값으로 나눠
         for(int i=1; i<=d && i<=n ; i++){
             if(d%i==0 && n%i==0){
                 max = i;
